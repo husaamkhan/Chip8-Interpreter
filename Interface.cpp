760fb2d043cd3652f177b4f7f73a553d47a16e93
Husaam Khan <husaamkhan04@gmail.com>
2024-12-31 13:49:38 -0500

    Implemented constructor and destructor

--- Interface.cpp
+++ Interface.cpp
@@ -1,8 +1,9 @@
 #include "Interface.h"
 #include <iostream>
+
 using namespace std;
 
-Interface::Interface()
+Interface::Interface(char* title, int w_window, int h_window, int w_texture, int h_texture)
 {
     if ( SDL_Init(SDL_INIT_VIDEO) < 0 )
     {
@@ -12,7 +13,16 @@ Interface::Interface()
     {
         cout << "SDL successfully initialized" << endl;
     }
+
+    window = SDL_CreateWindow(title, 0, 0, w_window, h_window, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, w_texture, h_texture);
 }
 
 Interface::~Interface()
-{}
+{
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
--- Interface.h
+++ Interface.h
@@ -1,3 +1,8 @@
+/*
+    --Interface class--
+    Purpose: Graphics rendering and keyboard input
+*/
+
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
@@ -6,8 +11,19 @@
 class Interface
 {
     public:
-        Interface();
+        Interface(char* title, int w_window, int h_window, int w_texture, int h_texture);
         ~Interface();
+
+        void refresh();
+
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_Texture* texture;
+
+        // The display that was originally supported by the Chip-8
+        const unsigned int X = 64;
+        const unsigned int Y = 32;
 };
 
 #endif // INTERFACE_H
